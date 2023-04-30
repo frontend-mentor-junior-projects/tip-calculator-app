38e53eae42f627f41a164443338b558e2d89372d
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-04-30 23:26:56 +0200

    chore: configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,74 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb-typescript',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 14,
+		sourceType: 'module',
+		project: './tsconfig.eslint.json',
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		// Disable React import.
+		'react/react-in-jsx-scope': 0,
+		// Sort imports.
+		'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					['external', 'builtin'],
+					'internal',
+					['sibling', 'parent'],
+					'index',
+				],
+				pathGroups: [
+					{
+						pattern: '@(react|react-native)',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: 'src/**',
+						group: 'internal',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['internal', 'react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+		// Require default props.
+		'react/require-default-props': [
+			'error',
+			{
+				forbidDefaultForRequired: true,
+				functions: 'defaultArguments',
+			},
+		],
+		// Allow use of arrow functions.
+		'arrow-body-style': ['warn', 'always'],
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: ['function-declaration', 'arrow-function'],
+				unnamedComponents: 'arrow-function',
+			},
+		],
+	},
 }
